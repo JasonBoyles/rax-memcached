f5c261210c7d98e090c0bd8f2e8b387967d1d61e
Jason Boyles <jason@duncancreek.net>
2014-03-26 09:51:34 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'rax-memcached'
+maintainer       'Rackspace'
+maintainer_email 'jason.boyles@rackspace.com'
+license          'All rights reserved'
+description      'Installs/Configures rax-memcached'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'memcached'
